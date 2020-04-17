cd705a70d8ff1321b4b80558408dcb6d9b065e58
aungwin <aungwinygn1990@gmail.com>
2020-04-17 14:26:54 +0630

    transfer from quee project

---
+++ src/components/layout/drawer/Tabs.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <q-tabs align="left" class="bg-primary text-white shadow-2">
+    <q-route-tab to="/page1" label="One" />
+    <q-route-tab to="/page2" label="Two" />
+    <q-route-tab to="/page3" label="Three" />
+  </q-tabs>
+</template>
+
+<script>
+export default {
+  name: 'Tab'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
